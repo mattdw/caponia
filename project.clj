5a638973c207a664788a9959aeda7feef1998fe8
Matt Wilson <matt@culturethree.com>
2023-01-17 17:49:14 +1300

    bump version and stemmers/clojure deps

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject caponia "0.3.3"
+(defproject caponia "0.3.4"
   :description "A simple in-memory full-text search library"
   :url "http://github.com/mattdw/caponia"
   :license "Eclipse Public License 1.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [stemmers "0.2.2"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [stemmers "0.2.3"]]
   :plugins [[lein-marginalia "0.8.0"]])
